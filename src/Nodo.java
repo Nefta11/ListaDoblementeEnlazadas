9485eeb53cb547126762bbb1efb2e44748c15b7a
Nefta11 <neftaliarturohernandez@gmail.com>
2023-11-09 16:58:23 -0600

    Siuuu

---
+++ src/Nodo.java
@@ -0,0 +1,20 @@
+public class Nodo {
+    
+    public int Dato;
+    Nodo siguiente, anterior;
+
+
+    //constructor para añadir un elemento al inicio
+    public Nodo(int d, Nodo s, Nodo a){
+        Dato =d;
+        siguiente= s;
+        anterior= a;
+
+    }
+    //constructor para cuando la lista está vacia
+
+    public Nodo(int d){
+        //Es lo mismo como si ousieramos this.dato=d, this.anterior=null, pero de manera mas simple.
+        this(d,null,null);
+    }
+}
