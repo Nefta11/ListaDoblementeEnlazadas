9eafbe4fe26356f7fc66964688cfefb80bc47fac
Nefta11 <neftaliarturohernandez@gmail.com>
2023-11-09 17:03:06 -0600

    Actualizado...

--- src/Lista.java
+++ src/Lista.java
@@ -1,5 +1,14 @@
 public class Lista {
-   
+    private Nodo inicio, fin;
+
+    public Lista() {
+        inicio = fin = null;
+    }
+
+    //Método para determinar si la lista está vacía
+
+    public boolean listaVacia(){
+        return inicio==fin;
+    }
 
 }
-
