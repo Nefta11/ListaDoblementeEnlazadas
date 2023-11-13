36c921180baf6c9d3e5cb8aeb42d12a425ec8b72
Nefta11 <neftaliarturohernandez@gmail.com>
2023-11-12 20:20:06 -0600

    sIUUU

--- src/ListaDoblementeEnlazadas.java
+++ src/ListaDoblementeEnlazadas.java
@@ -8,7 +8,9 @@ public class ListaDoblementeEnlazadas {
         int opcion = 0, el;
 
         do{
-            opcion=Integer.parseInt(JOptionPane.showInputDialog(null,"1. Agregar el valor del elemento\n2. Agregar el valor al final\n3. Eliminar inicio\n4. Eliminar Final\n5.Eliminar elemento espesifico\n6.Cerrar"));
+            opcion=Integer.parseInt(JOptionPane.showInputDialog(null,"1. Agregar el valor del elemento\n2. Agregar el valor al final\n3. Eliminar inicio\n4. Eliminar Final\n5.Eliminar elemento espesifico\\n" + //
+                    "6.-Buscar elemento especifico \\n" + //
+                    "7.- Actualizar elemento especifico\n8.Cerrar"));
             switch ((opcion)) {
                 case 1: el= Integer.parseInt(JOptionPane.showInputDialog(null,"Ingresa el valor del elemento"));
                     ldl.agregarInicio(el);
@@ -31,7 +33,16 @@ public class ListaDoblementeEnlazadas {
                     ldl.eliminarElementoE(el);
                     ldl.imprimir();
                     break;
-                case 6: break;
+                case 6:
+                    el = Integer.parseInt(JOptionPane.showInputDialog("Ingresa el dato a buscar"));
+                    lsl.buscarEspecifico(el);
+                    break;
+                case 7:
+                    e = Integer.parseInt(JOptionPane.showInputDialog("Ingresa el dato a buscar"));
+                    n = Integer.parseInt(JOptionPane.showInputDialog("Ingresa el Nuevo dato"));
+                    lsl.actualizarElemento(e, n);
+                    break;
+                case 8: break;
             
                 default: JOptionPane.showMessageDialog(null,"Opcion incorrecta bobo");
                     break;
